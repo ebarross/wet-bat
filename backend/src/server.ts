abaa2da2d138e02080c7d25e71f446f22c22d5d4
Emanoel Barros <ebarros297@gmail.com>
2022-11-16 19:24:30 -0300

    setup express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import express, { Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.get("/", (req, res: Response) => {
+  res.send("Wet Bat API");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server ready at: http://localhost:${port}`);
+});
