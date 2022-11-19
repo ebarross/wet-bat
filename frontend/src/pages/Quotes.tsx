6c91701d9326e20b1416b09f5101a70c9af2350a
Emanoel Barros <ebarros297@gmail.com>
2022-11-19 12:27:18 -0300

    list quotes on a page

--- frontend/src/pages/Quotes.tsx
+++ frontend/src/pages/Quotes.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import QuoteList from "../components/QuoteList";
+import useQuotes from "../hooks/useQuotes";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Message = styled.div`
+  margin-top: 60px;
+  font-size: 1.7rem;
+  color: #666;
+`;
 
 function Quotes() {
+  const { data, error, loading } = useQuotes();
+
   return (
-    <div>
-      <h1>Quotes</h1>
-    </div>
+    <Container>
+      {loading ? (
+        <Message>Loading quotes...</Message>
+      ) : error || !data ? (
+        <Message>Error or fetching quotes. Try again.</Message>
+      ) : (
+        <QuoteList list={data} />
+      )}
+    </Container>
   );
 }
 
